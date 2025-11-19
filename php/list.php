4d36b530d464b07dfe40503d3cf077d2572f7feb
smallru8 <30855604+smallru8@users.noreply.github.com>
2025-11-20 05:35:55 +0800

    Update list.php

--- php/list.php
+++ php/list.php
@@ -13,9 +13,12 @@
       $dir_path = "/var/www/html/list/";
       $files = scandir($dir_path, SCANDIR_SORT_DESCENDING);
       foreach($files AS $f_name){
+        if(str_starts_with($f_name,".")){
+          continue;
+        }
     		?>
         <p><a href="/list/<?php echo $f_name; ?>">
-        <?php echo $f_name; ?> - <?php filemtime($dir_path.$f_name); ?>
+        <?php echo $f_name; ?> - <?php echo date("Y-m-d H:i:s", filemtime($dir_path.$f_name)); ?>
         </a></p>
     <?php
     	}
