5749733780ea87134a0d63032eb727cfda5cf246
smallru8 <30855604+smallru8@users.noreply.github.com>
2025-11-20 05:31:20 +0800

    Update list.php

--- php/list.php
+++ php/list.php
@@ -5,14 +5,17 @@
     </title>
   </head>
   <body>
-    <h1>List</h1>
+    <h1>Files</h1>
+    <h2>Latest</h2>
+    <p><a href="/latest.php">Download Latest</a></p>
+    <h2>List</h2>
     <?php
       $dir_path = "/var/www/html/list/";
       $files = scandir($dir_path, SCANDIR_SORT_DESCENDING);
       foreach($files AS $f_name){
     		?>
         <p><a href="/list/<?php echo $f_name; ?>">
-        <?php echo $f_name; ?>
+        <?php echo $f_name; ?> - <?php filemtime($dir_path.$f_name); ?>
         </a></p>
     <?php
     	}
